61e84f54cea6399f7754180cff0bb6de85ae3bc9
maximedoaw <maximedoaw204@gmail.com>
2024-09-09 11:45:38 +0100

    update detail page of post

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,8 +1,24 @@
+import { db } from "@/firebase"
+import { doc, getDoc } from "firebase/firestore"
+import { HiArrowLeft } from "react-icons/hi";
+import Link from "next/link";
+import Post from "@/app/components/Post";
 
-function page() {
+async function page({params}) {
+  let data = {}
+  const querySnapshot = await getDoc(doc(db,'posts',params.id))
+  data = { ...querySnapshot.data(),id: querySnapshot.id }
+
   return (
-    <div>
-      PostPage
+    <div className="max-w-xl mx-auto border-r border-l min-h-screen">
+      <div className='flex items-center space-x-2 py-2 px-3 sticky top-0 z-50
+      bg-white border-b border-gray-200'>
+        <Link href={'/'} className="hover:bg-gray-100 rounded-full p-2">
+          <HiArrowLeft className="h-5 w-5 cursor-pointer" />
+        </Link>
+        <h2 className="sm:text-lg">Back</h2>
+      </div>
+      <Post post={data} id={data.id}/>
     </div>
   )
 }
